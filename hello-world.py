afbd31b1969b109d3ee80e06ca36a972eb9c8b12
amccartney <allison.n.mccartney@gmail.com>
2015-04-06 11:21:34 -0700

    intro py script

---
+++ hello-world.py
@@ -0,0 +1,9 @@
+print "Hello world"
+myname = "Allison"
+print "My name is", myname 
+# Note: probably shouldn't put your real age here if your
+# repo is going to be posted online
+myage = 31
+print "I am", myage, "years old"
+print "That is roughly", 31 * 365, "days" 
+print "Or,", 31 * 365 * 24 * 60 * 60, "seconds"  
