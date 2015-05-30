60c19d4d09ca13edaeff5fdf6585c11d76b51315
amccartney <allison.n.mccartney@gmail.com>
2015-05-30 16:11:36 -0700

    progress!

--- final-project/app.py
+++ final-project/app.py
@@ -1,4 +1,4 @@
-import json
+import json, operator, requests
 from flask import Flask 
 from flask import abort
 from flask import render_template
@@ -17,23 +17,41 @@ hp_data = data['usaspendingSearchResults']['result']['doc']
 agencies = {}
 for h in hp_data:
     agency = h['AgencyID']
+    contract_amount = float(h['DollarsObligated'])
     if agency in agencies:
-        agencies[agency] += 1
+        agencies[agency] += contract_amount
     else:
-        agencies[agency] = 1    
+        agencies[agency] = contract_amount   
 
 # print(agencies)
 
 # CONTRACTS BY RECIPIENT CITY
-recipient_city = {}
+recipient_city = []
+cities = []
 for h in hp_data:
-    city_state = str(h['RecipientCity'] + "," + h['RecipientState'])
-    if city_state in recipient_city:
-        recipient_city[city_state] += 1
-    else:
-        recipient_city[city_state] = 1    
+    city_state = str(h['RecipientCity'] + ", " + h['RecipientState'])
+    contract_amount = int(float(h['DollarsObligated']))
+    if city_state not in cities:
+        recipient_city.append([city_state, contract_amount, 1, 'x', 'y'])
+        cities.append(city_state)   
+    else: 
+        for r in recipient_city:
+            if r[0] == city_state:
+                r[1] += contract_amount
+                r[2] += 1 
 
-# print(recipient_city)
+recipient_city = sorted(recipient_city, key=operator.itemgetter(1), reverse=True)
+
+# GEOCODING
+maps_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+for r in recipient_city:
+    atts = {'address': r[0], 'key': 'AIzaSyDdBsQKgsXLrHAil_feh5RK_g5lyxbRmAU'}
+    resp = requests.get(maps_url, params = atts)
+    data = resp.json()
+    r[3] = data['results'][0]['geometry']['location']['lat']
+    r[4] = data['results'][0]['geometry']['location']['lng']
+
+print(recipient_city)
 
 # CONTRACTS BY PERFORMANCE CITY
 '''
@@ -66,16 +84,22 @@ for h in hp_data:
 # print(fiscal_year)
 
 # DOLLARS OBLIGATED BY PRODUCTS OR SERVICE CODE
-products_code = {}
+products_code = []
+codes = []
 for h in hp_data:
     code = h['ProductorServiceCode']
-    contract_amount = float(h['DollarsObligated'])
-    if code in products_code:
-        products_code[code] += contract_amount
-    else:
-        products_code[code] = contract_amount
+    contract_amount = int(float(h['DollarsObligated'])) 
+    if code not in codes:
+        products_code.append([code, contract_amount, 1])
+        codes.append(code)   
+    else: 
+        for p in products_code:
+            if p[0] == code:
+                p[1] += contract_amount
+                p[2] += 1 
+        
+products_code = sorted(products_code, key=operator.itemgetter(1), reverse=True)
 
-# print(products_code)
 
 
 
@@ -83,8 +107,9 @@ for h in hp_data:
 def index():
     template = 'index.html'
     object_list = hp_data
-    return render_template(template, object_list=object_list)
-
+    products_list = products_code
+    recipient_city_list = recipient_city
+    return render_template(template, object_list=object_list, products_list=products_list, recipient_city_list=recipient_city_list)
 
 '''
 @app.route('/<row_id>/')
